008b356f1f872da465d1e9e443ee7fc60ede8806
i-obr <obr.iv@ya.ru>
2018-07-27 10:44:24 +0300

    feature(fetch + json-server)

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
 import UserList from '../UserList';
-import users from '../../fixtures';
 import './App.css';
 
+const API = 'http://localhost:3001/users'
+
 class App extends Component {
+  state = {
+    users: null,
+    error: '',
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Something went wrong ...');
+        }
+      })
+      .then(data => {
+        this.setState({
+          users: data,
+        });
+      })
+      .catch(error => this.setState({ error }));
+  }
+
   render() {
+    const { users, error } = this.state;
+
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+
     return (
       <div className="content">
         <UserList data = { users } />
